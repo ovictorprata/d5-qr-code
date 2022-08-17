dac0381f49bc97536a32a89eea961e3f54ffddd2
Victor Prata <victor.prata@buser.com.br>
2022-08-17 18:07:58 -0300

    mudando o meu código para o código da batata

--- workspace/javascript/teoria_roger/exercicios/wordle/versao_com_div/script.js
+++ workspace/javascript/teoria_roger/exercicios/wordle/versao_com_div/script.js
@@ -1,24 +1,50 @@
-var div1 = document.getElementById("div1");
-var div2 = document.getElementById("div2");
-var div3 = document.getElementById("div3");
-var div4 = document.getElementById("div4");
-var div5 = document.getElementById("div5");
-var jogo = document.querySelector("div.jogo");
-let lista = [];
+let lista = ["", "", "", "", ""];
+let tentativas = [];
+let i = 0;
+
+let variaveis = document.querySelectorAll("div.caixa-jogo");
 
 function PegarTecla(caractere) {
-  console.log(caractere);
-  if (lista.length <= 4 && caractere.keyCode != 8) {
-    lista.push(String.fromCharCode(caractere.keyCode).toUpperCase());
+  if (tentativas.length == 0) {
+    // se lenght == 0 e backpase OU lenght = 5 e letra --> return
+    if (lista[0] == "" && caractere.keyCode == 8) {
+      return;
+    } else if (caractere.keyCode == 8 && i >= 0) {
+      lista[--i] = "";
+      console.log(i);
+    } else if (i < 5 && i >= 0) {
+      //lista[i++] = String.fromCharCode(caractere.keyCode).toUpperCase();
+      lista[i++] = caractere.key.toUpperCase();
+      console.log(i);
+    } else if (i == 5 && caractere.keyCode == 13) {
+      console.log(i);
+      tentativas.push(lista.join(""));
+    }
+    variaveis[0].innerHTML = lista[0];
+    variaveis[1].innerHTML = lista[1];
+    variaveis[2].innerHTML = lista[2];
+    variaveis[3].innerHTML = lista[3];
+    variaveis[4].innerHTML = lista[4];
+  } else if (tentativas.length == 1) {
+    // se lenght == 0 e backpase OU lenght = 5 e letra --> return
+    if (lista[0] == "" && caractere.keyCode == 8) {
+      return;
+    } else if (caractere.keyCode == 8 && i >= 0) {
+      lista[--i] = "";
+      console.log(i);
+    } else if (i < 5 && i >= 0) {
+      //lista[i++] = String.fromCharCode(caractere.keyCode).toUpperCase();
+      lista[i++] = caractere.key.toUpperCase();
+      console.log(i);
+    } else if (i == 5 && caractere.keyCode == 13) {
+      console.log(i);
+      tentativas.push(lista.join(""));
+    }
+    div1.innerHTML = lista[0];
+    div2.innerHTML = lista[1];
+    div3.innerHTML = lista[2];
+    div4.innerHTML = lista[3];
+    div5.innerHTML = lista[4];
   }
-  if (caractere.keyCode == 8) {
-    lista.pop();
-    // lista[lista.length - 1] = "";
-  }
-
-  div1.innerHTML = lista;
-
-  // div1.innerHTML = String.fromCharCode(event.keyCode).toUpperCase();
+  l2c1.innerHTML = tentativas;
 }
-
-// You can do the same with all the other keys. No input tag needed.
